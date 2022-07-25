5cb76b3d4d8d6b0d358c3fb17b3a320e39ee091f
alekpermanaa <1441177004095@student.unsika.ac.id>
2022-07-25 15:58:51 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide')
+})
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("pict");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "flex";  
+}
